a2abef2ad6faf8be8e0b4b1dcf68d4ac7435a4ca
Riveros <df.riveros11@uniandes.edu.co>
2021-09-14 09:53:33 -0700

    Chang main.dart return UI

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -79,8 +79,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // return LoginScreen();
-
-    return MainScreen();
+    return LoginScreen();
   }
 }
